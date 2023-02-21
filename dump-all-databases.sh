41c3e4c3444cbeffbf94528db17e29bab92dd183
Olivier GBLEM <olivier@keographix.com>
2023-02-21 17:42:45 +0000

    ♻️ Update php-cs-fixer rules and db dump cmd

--- dump-all-databases.sh
+++ dump-all-databases.sh
@@ -1,1 +1,3 @@
 mysql -u root -p -N -e 'show databases' | while read dbname; do mysqldump --complete-insert --force -u root -p  "$dbname" > "$dbname".sql; [[ $? -eq 0 ]] && gzip "$dbname".sql; done
+
+mysqldump --complete-insert --force -u root -p  "mlm_116" > "mlm_116".sql;
